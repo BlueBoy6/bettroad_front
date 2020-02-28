77dcb2f83fe180693cdfd116c52a74a8bacbe9b1
davidsi <davidrafael.simoessilva@gmail.com>
2020-02-28 14:52:34 +0100

    change env from prod to local | change data structure of posts

--- src/helpers/betsdata.js
+++ src/helpers/betsdata.js
@@ -1,122 +1,102 @@
 /* eslint-disable no-console */
-import axios from 'axios';
-import { back } from './constants';
+import axios from "axios";
+import { back } from "./constants";
 
 const backurl = back.url;
 
 export const getBetsType = token => {
-	return axios
-		.get(`${backurl}/betstypes`, {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		})
-		.then(result => {
-			const betsTypes = result.data;
-			return betsTypes;
-		})
-		.catch(err => {
-			console.log('=================');
-			console.log(err);
-			return err;
-		});
+  return axios
+    .get(`${backurl}/betstypes`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(result => {
+      const betsTypes = result.data;
+      return betsTypes;
+    })
+    .catch(err => {
+      console.log("=================");
+      console.log(err);
+      return err;
+    });
 };
 
 export const getTeammates = token => {
-	return axios
-		.get(`${backurl}/players`, {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		})
-		.then(result => {
-			const players = result.data.map(player => {
-				return { id: player.id, name: player.name };
-			});
-			return players;
-		})
-		.catch(err => {
-			console.log('=================');
-			console.log(err);
-			return err;
-		});
+  return axios
+    .get(`${backurl}/players`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(result => {
+      const players = result.data.map(player => {
+        return { id: player.id, name: player.name };
+      });
+      return players;
+    })
+    .catch(err => {
+      console.log("=================");
+      console.log(err);
+      return err;
+    });
 };
 export const getTeams = token => {
-	return axios
-		.get(`${backurl}/teams`, {
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		})
-		.then(result => {
-			const teams = result.data.map(team => {
-				return { id: team.id, name: team.name, city: team.city };
-			});
-			return teams;
-		})
-		.catch(err => {
-			console.log('=================');
-			console.log(err);
-			return err;
-		});
+  return axios
+    .get(`${backurl}/teams`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(result => {
+      const teams = result.data.map(team => {
+        return { id: team.id, name: team.name, city: team.city };
+      });
+      return teams;
+    })
+    .catch(err => {
+      console.log("=================");
+      console.log(err);
+      return err;
+    });
 };
 
 export const getBets = async (token, idBetter) => {
-	try {
-		const getBets = await axios({
-			method: 'get',
-			url: `${backurl}/bets?user=${idBetter}`,
-			headers: {
-				Authorization: `Bearer ${token}`
-			}
-		});
-		if (getBets.statusText === 'OK') {
-			return getBets.data;
-		}
-	} catch (err) {
-		console.log('error  :', err);
-	}
+  try {
+    const getBets = await axios({
+      method: "get",
+      url: `${backurl}/bets?user=${idBetter}`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    if (getBets.statusText === "OK") {
+      return getBets.data;
+    }
+  } catch (err) {
+    console.log("error  :", err);
+  }
 };
 export const postBet = async (token, datas) => {
-	try {
-		// let responses = [];
-		console.log('datas', datas);
+  try {
+    // let responses = [];
+    console.log("datas", datas);
 
-		const axcall = await axios({
-			method: 'post',
-			url: `${backurl}/bets`,
-			headers: {
-				Authorization: `Bearer ${token}`
-			},
-			data: {
-				...datas
-			}
-		});
-		if (axcall) {
-			console.log('what about return : ', axcall);
-		}
-
-		// for (let i = 0; i < datas.length; ) {
-		// 	const axcall = await axios({
-		// 		method: 'post',
-		// 		url: `${backurl}/bets`,
-		// 		headers: {
-		// 			Authorization: `Bearer ${token}`
-		// 		},
-		// 		data: {
-		// 			...datas[i]
-		// 		}
-		// 	});
-		// 	if (axcall.statusText === 'OK') {
-		// 		responses.push(axcall);
-		// 		i++;
-		// 	}
-		// }
-		// if (responses.length === datas.length) {
-		// 	console.log('happy call !');
-		// 	return { status: 'OK', data: responses };
-		// }
-	} catch (err) {
-		console.log('error  :', err);
-	}
+    const betcall = await axios({
+      method: "post",
+      url: `${backurl}/bets`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      data: {
+        ...datas
+      }
+    });
+    if (betcall) {
+      console.log("what about return : ", betcall);
+      return { status: "OK", data: betcall.data };
+    }
+  } catch (err) {
+    console.log("error  :", err);
+  }
 };
--- src/store/postBetAction.js
+++ src/store/postBetAction.js
@@ -1,38 +1,30 @@
-import { postBet } from '../helpers/betsdata';
+import { postBet } from "../helpers/betsdata";
 export const postBets = async function(context, payload) {
-	const dataBets = payload.bets;
-	const gamedayDate = payload.gameday.id;
-	// const dataToSubmit = dataBets.map(bet => {
-	// 	return {
-	// 		user: context.state.user.id,
-	// 		userName: context.state.user.name,
-	// 		betSubmited: bet.value,
-	// 		gameday: gamedayDate,
-	// 		betstype: bet.idBet
-	// 	};
-	// });
-	const tryPost = {
-		user: context.state.user.id,
-		userName: context.state.user.name,
-		gameday: gamedayDate,
-		betsSubmited_TEST: dataBets.map(v => {
-			const betssubmit = Array.isArray(v.value) ? v.value[0] : v.value;
-			return { __component: v.type, result: betssubmit, label: v.label };
-		})
-	};
-	const postAction = await postBet(context.state.user.token, tryPost);
-	if (postAction.status === 'OK') {
-		const nextGameId = context.state.gamedays.nextGame.id;
-		const betOfNextGame = postAction.data.filter(
-			bet => bet.data.gameday.id === nextGameId
-		);
-		const nextGameBetSubmited = betOfNextGame.map(bet => {
-			return {
-				betType: bet.data.betstype.name,
-				betSubmited: bet.data.betSubmited
-			};
-		});
-		context.commit('postBetCommit', nextGameBetSubmited);
-		return nextGameBetSubmited;
-	}
+  const dataBets = payload.bets;
+  const gamedayDate = payload.gameday.id;
+  const tryPost = {
+    user: context.state.user.id,
+    userName: context.state.user.name,
+    gameday: gamedayDate,
+    betsSubmited_TEST: dataBets.map(v => {
+      const betssubmit = Array.isArray(v.value) ? v.value[0] : v.value;
+      return { __component: v.type, result: betssubmit, label: v.label };
+    })
+  };
+  const postAction = await postBet(context.state.user.token, tryPost);
+  if (postAction.status === "OK") {
+    const nextGameId = context.state.gamedays.nextGame.id;
+    // REWORK NECESSARY TO PUSH RESULTS ON SAVES SUBMITEDS
+    const betOfNextGame = postAction.data.betsSubmited_TEST.filter(
+      bet => bet.data.gameday.id === nextGameId
+    );
+    const nextGameBetSubmited = betOfNextGame.map(bet => {
+      return {
+        betType: bet.data.betstype.name,
+        betSubmited: bet.data.betSubmited
+      };
+    });
+    context.commit("postBetCommit", nextGameBetSubmited);
+    return nextGameBetSubmited;
+  }
 };
