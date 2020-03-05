0a179325235b45c87117c8d132695e5e32104474
dsimoessilva <dsimoessilva@mail.fr>
2020-03-05 14:07:57 +0100

    vue config pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+	transpileDependencies: ["vuetify"],
+	pwa: {
+		name: "Bettroad App - Paris là !",
+		themeColor: "#141414",
+		msTileColor: "#ffffff",
+		appleMobileWebAppCapable: "yes",
+		appleMobileWebAppStatusBarStyle: "black",
+		iconPaths: {
+			favicon32: "/favicon.ico",
+			favicon16: "/favicon.ico",
+			appleTouchIcon: "/favicon.ico",
+			maskIcon: "/favicon.ico",
+			msTileImage: "/favicon.ico"
+		},
+		// configure the workbox plugin
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: "dev/sw.js"
+			// ...other Workbox options...
+		}
+	}
+};
