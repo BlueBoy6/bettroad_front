d85a4843697437c5e321e08248f5cc0dea57db2b
BlueBoy <davidrafael.simoessilva@gmail.com>
2020-03-08 00:18:02 +0100

    added user error user message in login page

--- src/components/pages/LoginPage.vue
+++ src/components/pages/LoginPage.vue
@@ -1,7 +1,12 @@
 <template>
 	<v-container class="modal__container">
 		<v-row align="center" justify="center" class="modal__connect__row">
-			<v-col cols="10" sm="6" md="8" class="grey darken-4 pa-sm-3 pa-md-10 modal__connect">
+			<v-col
+				cols="10"
+				sm="6"
+				md="8"
+				class="grey darken-4 pa-sm-3 pa-md-10 modal__connect"
+			>
 				<v-text-field
 					@input="val => (login = val)"
 					:color="colorInputs"
@@ -18,18 +23,32 @@
 					@keydown="keyDownSwitcher"
 					outlined
 				></v-text-field>
+				<v-alert
+					v-model="error.status"
+					close-text="Close Alert"
+					:class="colorErrorModal"
+					dark
+					dismissible
+					>{{ error.message }}</v-alert
+				>
 				<v-row justify="end">
 					<v-col cols="auto" class="py-0">
-						<v-btn class="" :class="colorBtn" @click="connect" large float-right> 
+						<v-btn
+							class=""
+							:class="colorBtn"
+							@click="connect"
+							large
+							float-right
+						>
 							Go parier !
 							<v-progress-circular
-							v-if="waitForLog"
+								v-if="waitForLog"
 								:size="10"
 								:width="2"
 								:color="colorBackgroundLight"
 								indeterminate
 							/>
-							</v-btn>
+						</v-btn>
 					</v-col>
 				</v-row>
 			</v-col>
@@ -39,37 +58,56 @@
 
 <script>
 /* eslint-disable no-console */
-import { colorInputs, colorBtn, colorBackgroundLight } from '../../style/colors.vars';
+import {
+	colorInputs,
+	colorBtn,
+	colorBackgroundLight,
+	colorBackgroundDark,
+	colorErrorModal
+} from '../../style/colors.vars';
 import { loginConnect } from '../../helpers/login';
 export default {
 	data() {
 		return {
 			colorInputs,
 			colorBtn,
+			colorErrorModal,
 			colorBackgroundLight,
+			colorBackgroundDark,
 			login: '',
 			password: '',
 			showMdp: false,
-			waitForLog: false
+			waitForLog: false,
+			error: {
+				status: false,
+				message: 'Ton mot de passe ou ton identifiant est incorrect !'
+			}
 		};
 	},
 	methods: {
-		keyDownSwitcher: function(e){
-			if(e.key === "Enter") return this.connect()
+		keyDownSwitcher: function(e) {
+			if (e.key === 'Enter') return this.connect();
 		},
 		connect: async function() {
 			this.waitForLog = true;
 			const connection = await loginConnect(this.login, this.password);
+			console.log('connection', connection);
 			if (connection.status === 'OK') {
 				this.$store.commit('login', connection.user);
 				this.$router.push({
 					path: '/dashboard'
 				});
+			} else {
+				this.waitForLog = false;
+				this.error.status = true;
 			}
 		}
 	},
-	mounted(){
-		if(localStorage.userId !== undefined && localStorage.userToken !== undefined){
+	mounted() {
+		if (
+			localStorage.userId !== undefined &&
+			localStorage.userToken !== undefined
+		) {
 			this.$router.push({
 				path: '/dashboard'
 			});
@@ -85,6 +123,7 @@ export default {
 		height: 100%;
 		.modal__connect {
 			border-radius: 4px;
+			position: relative;
 		}
 	}
 }
