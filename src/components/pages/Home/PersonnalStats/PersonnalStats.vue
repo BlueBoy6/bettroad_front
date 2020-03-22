77bb0b76713b48c532d847d1cc55672fe70fd181
BlueBoy <davidrafael.simoessilva@gmail.com>
2020-03-22 16:22:30 +0100

    small fix nextgame case no bet exist

--- src/components/pages/Home/PersonnalStats/PersonnalStats.vue
+++ src/components/pages/Home/PersonnalStats/PersonnalStats.vue
@@ -1,40 +1,39 @@
 <template>
-	<v-sheet
-		class="mb-5 mt-5"
-		:class="[spaceInside, colorBackgroundLight, darkText]"
-	>
-		<h2>Statistiques personnels</h2>
-		<v-sparkline
-			:value="pgPercent"
-			color="#00897b"
-			height="100"
-			padding="20"
-			stroke-linecap="round"
-			smooth
-		>
-		</v-sparkline>
-		<v-row>
-			<v-col
-				align="center"
-				justify="center"
-				class="px-1 px-lg-4"
-				:key="pg.day.dayNumber"
-				v-for="pg in pgData"
+	<div class="my-5">
+		<p class="display-1 mb-5" :class="[whiteText]">Tes statistiques</p>
+		<v-sheet :class="[spaceInside, colorBackgroundLight, darkText]">
+			<v-sparkline
+				:value="pgPercent"
+				color="#00897b"
+				height="100"
+				padding="20"
+				stroke-linecap="round"
+				smooth
 			>
-				<v-tooltip :position-y="1" bottom :color="colorBackgroundDark">
-					<template v-slot:activator="{ on }">
-						<div v-on="on" class="pa-1 pa-lg-4" :class="[darkText]">
-							<span>J{{ pg.day.dayNumber }}</span>
+			</v-sparkline>
+			<v-row>
+				<v-col
+					align="center"
+					justify="center"
+					class="px-1 px-lg-4"
+					:key="pg.day.dayNumber"
+					v-for="pg in pgData"
+				>
+					<v-tooltip :position-y="1" bottom :color="colorBackgroundDark">
+						<template v-slot:activator="{ on }">
+							<div v-on="on" class="pa-1 pa-lg-4" :class="[darkText]">
+								<span>J{{ pg.day.dayNumber }}</span>
+							</div>
+						</template>
+						<div align="center" justify="center">
+							<div>{{ pg.day.fr }}</div>
+							<div>{{ pg.city }}</div>
 						</div>
-					</template>
-					<div align="center" justify="center">
-						<div>{{ pg.day.fr }}</div>
-						<div>{{ pg.city }}</div>
-					</div>
-				</v-tooltip>
-			</v-col>
-		</v-row>
-	</v-sheet>
+					</v-tooltip>
+				</v-col>
+			</v-row>
+		</v-sheet>
+	</div>
 </template>
 
 <script>
