093ee1ea9b5dd9e814cbbfb7adb12f8e3aac09fe
BlueBoy <davidrafael.simoessilva@gmail.com>
2020-03-22 14:59:35 +0100

    refacto sort of gamedays

--- src/store/getGamedays.js
+++ src/store/getGamedays.js
@@ -1,37 +1,37 @@
 /* eslint-disable no-console */
-import { getGamedays as getCalendar } from "../helpers/calendar";
-import { dateFormater } from "../helpers/calendar";
-import moment from "moment";
-import momentfr from "../helpers/momentfr";
+import { getGamedays as getCalendar } from '../helpers/calendar';
+import { dateFormater } from '../helpers/calendar';
+import moment from 'moment';
+import momentfr from '../helpers/momentfr';
 
 export const getGamedays = async function(context) {
 	momentfr;
-	const games = await getCalendar(context.state.user.token);
-	if (games) {
-		const pastGames = games
-			.filter(date => {
-				return moment(date.day).isBefore(Date.now()) && date;
-			})
-			.sort((a, b) => Date.parse(b.day) - Date.parse(a.day))
-			.map(date => {
-				return { ...date, day: dateFormater(date.day) };
+	try {
+		const games = await getCalendar(context.state.user.token);
+		if (games) {
+			const gamesSortedFormated = games
+				.sort((a, b) => Date.parse(b.day) - Date.parse(a.day))
+				.map(date => {
+					return { ...date, day: dateFormater(date.day) };
+				});
+			console.log('gamesSorted : ', gamesSortedFormated);
+
+			const pastGames = gamesSortedFormated.filter(date => {
+				return moment(date.day.en).isBefore(Date.now()) && date;
 			});
 
-		const futureGames = games
-			.filter(date => {
-				return moment(date.day).isAfter(Date.now()) && date;
-			})
-			.sort((a, b) => Date.parse(a.day) - Date.parse(b.day))
-			.map(date => {
-				return { ...date, day: dateFormater(date.day) };
+			const futureGames = gamesSortedFormated.filter(date => {
+				return moment(date.day.en).isAfter(Date.now()) && date;
 			});
-
-		const storeGames = {
-			nextGame: futureGames[0],
-			futureGames: futureGames.splice(1, futureGames.length),
-			pastGames: pastGames
-		};
-		context.commit("storeGamedays", storeGames);
-		return { statusText: "OK" };
+			const storeGames = {
+				nextGame: futureGames[0],
+				futureGames: futureGames.splice(1, futureGames.length),
+				pastGames: pastGames
+			};
+			context.commit('storeGamedays', storeGames);
+			return { statusText: 'OK' };
+		}
+	} catch (err) {
+		throw "Nous n'avons pas réussi à récupérer les jours de matchs";
 	}
 };
